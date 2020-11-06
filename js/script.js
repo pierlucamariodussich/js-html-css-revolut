1b456247a4b371dd24aca27bbecd8413b929039c
pierlucamario_dussich <pierluca.dussich@gmail.com>
2020-11-06 17:59:55 +0100

    REVOLUT | version 1.0

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
-  $('.content').click(function(){
-    $(this).siblings('slot').children(".slot").removeClass('dropdown');
-    $(this).children(".slot").toggleClass('dropdown');
+
+  $('.menu > ul > li').click(function(){
+    console.log('ciao');
+
+    if( $(this).children('.dropdown').hasClass('active')){
+      $(this).children('.dropdown').removeClass('active');
+    } else {
+      $('.dropdown').removeClass('active');
+      $(this).children('.dropdown').addClass('active');
+      
+    }
+
   })
+
 })
