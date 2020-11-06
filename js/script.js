67c3570a9cdac11c61a810a51315a6ea03fe5dc4
pierlucamario_dussich <pierluca.dussich@gmail.com>
2020-11-06 17:13:29 +0100

    REVOLUT |

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
 $(document).ready(function(){
-
-
-
-
-
-
-
-
-
-
-
-
-  
+  $('.content').click(function(){
+    $(this).siblings('slot').children(".slot").removeClass('dropdown');
+    $(this).children(".slot").toggleClass('dropdown');
+  })
 })
